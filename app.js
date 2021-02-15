3aca37edad5a8a97542e3b13546a8d2ac7f3d740
L3p1989 <phogan110@live.com>
2021-02-15 14:05:50 -0700

    commented

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
+// assigns myName a string of L3p
 var myName = "L3p";
-
+// logs myName
 console.log(myName);
-
+// assigns const allStaes a numeric value of 50
 const allStates = 50;
-
+// logs allStates
 console.log(allStates);
-
+// assigns x numeric value of 5
 let x = 5;
-
+// assigns y numeric value of 4
 let y = 4;
-
+// assigns sum the value of x + y
 var sum = x + y;
-
+// logs sum
 console.log(sum);
